9ede9a34e41f7c7ec3584db7af279d9f60bfa678
Aryanzudaf <aryanzf1@gmail.com>
2024-09-10 11:30:02 +0700

    commit 10sep

--- TugasMing2.java
+++ TugasMing2.java
@@ -1,7 +1,12 @@
 public class TugasMing2 {
 
     public static void main(String[] args) {
-        System.out.println("Biodata Mahasiswa");
-
+        System.out.println("Nama\t:Aryan Zuda Firdaus");
+        System.out.println("Kelas\t:1A");
+        System.out.println("Absen\t:08");
+        System.out.println("Jurusan\t:Teknologi Informasi");
+        System.out.println("Prodi\t:Teknik Informatika");
+        System.out.println("TglLahir:21 juni 2006");
+        System.out.println("Hobi\t:Menyanyi");
     }
 }
