4e52e4e1d792e51a3865de6eaa051133ec32b62d
ilham <meniandol@gmail.com>
2024-09-10 16:10:33 +0700

    java

--- TugasMing2.java
+++ TugasMing2.java
@@ -3,13 +3,13 @@ public class TugasMing2 {
     public static void main(String[] args) {
         System.out.println("Biodata Mahasiswa");
 
-        System.out.println("Nama\t:Amin Aziz Sudjud");
+        System.out.println("Nama\t:Ilham Dharma Atmaja");
         System.out.println("Kelas\t:1A");
-        System.out.println("Absen\t:05");
+        System.out.println("Absen\t:14");
         System.out.println("Jurusan\t:Teknologi Informasi");
         System.out.println("Prodi\t:Teknik Informatika");
-        System.out.println("TglLahir:29 Desember 2005");
-        System.out.println("Hobi\t:Bermain & Olahraga");
+        System.out.println("TglLahir:10 Mei 2006");
+        System.out.println("Hobi\t:Voli & Mendaki");
 
 
 
